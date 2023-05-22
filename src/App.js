f5654b2a8ee41f8c48e417b2c7332bd67ce6c4a5
Jaimy Renji <jaimyrenji@gmail.com>
2023-05-22 14:29:00 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Landing from "./pages/home/Landing";
+import Login from "./pages/login/login";
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+function App(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/login" element={<Login/>}/>
+        </Routes>
+        </BrowserRouter>
+        
+      );
+    }
 export default App;
