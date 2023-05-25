3b705decc645ff758d7b6a2b5d0520e0f898f060
Jaimy Renji <jaimyrenji@gmail.com>
2023-05-25 11:00:07 +0530

    changes

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import "./login.css";
 import logo from "../../images/logo.png";
 import {LoginSocialGoogle} from "reactjs-social-login";
 import{useNavigate} from "react-router-dom";
 export default function Login() {
     const navigate=useNavigate();
+    const [name, setName] = useState("");
+    const [photo, setPhoto] = useState("");
+    const handleLoginResolve = ({ provider, data }) => {
+        console.log(provider, data);
+        const profile = data.profile || data;
+    const givenName = profile.given_name || profile.name || "";
+    const picture = profile.picture || "";
+
+    setName(givenName);
+    setPhoto(picture);
+    localStorage.setItem("name", givenName);
+    localStorage.setItem("photo", picture);
+    navigate("/dashboard");
+      };
+    
+      const handleLoginReject = (err) => {
+        console.log(err);
+      };
+
     return (
         <div className="login">
             <div className="image-overlay">
@@ -18,13 +37,8 @@ export default function Login() {
             scope="openid profile email"
             discoveryDocs="claims_supported"
             access_type="offline"
-            onResolve={({provider,data})=>{
-                console.log(provider,data);
-                navigate("/dashboard")
-            }}
-            onReject={(err)=>{
-                console.log(err);
-            }}
+            onResolve={handleLoginResolve}
+            onReject={handleLoginReject}
             >
                 <button className="login-btn"><div className="login-div"><img className="login-img"src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"></img>
                 <p><b>Sign up with google</b></p></div></button>
